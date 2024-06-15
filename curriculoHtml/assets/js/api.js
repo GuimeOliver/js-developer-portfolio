a4cc4b024c8086d97af0cd4c9fde27c382472a2d
GuimeOliver <163361623+GuimeOliver@users.noreply.github.com>
2024-06-14 21:27:32 -0300

    Delete curriculoHtml directory

--- curriculoHtml/assets/js/api.js
+++
@@ -1,6 +0,0 @@
-async function fetchProfileData() {
-    const url = 'https://guimeoliver.github.io/API-portifolio/db.json'
-    const fetching =  await fetch(url)
-    return await fetching.json()
-}
-
